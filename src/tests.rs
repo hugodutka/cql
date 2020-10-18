b3bdbb2dcf0d6bf2158eb9cc23f469488362b5a9
Hugo Dutka <contact@hugodutka.com>
2020-10-18 12:04:40 +0200

    add test for checking if connection fails

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,19 @@
-/// Note: these tests expect a CQL compatible database to be available on `localhost:9042`.
+/// Note: these tests expect a CQL compatible database to be available on `localhost:9042`. They
+/// Also expect that one will not be present at `localhost:14141`.
 use crate::*;
 
 const DATABASE_URL: &str = "localhost:9042";
+const NONEXISTENT_DATABASE_URL: &str = "localhost:14141";
 
 #[tokio::test]
 async fn can_initialize_connection() {
     Connection::new(DATABASE_URL).await.unwrap();
+}
+
+#[tokio::test]
+#[should_panic]
+async fn connection_initialization_fails_when_db_does_not_exist() {
+    Connection::new(NONEXISTENT_DATABASE_URL).await.unwrap();
 }
 
 #[tokio::test]
